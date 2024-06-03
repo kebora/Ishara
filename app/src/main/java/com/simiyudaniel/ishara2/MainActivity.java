598ba1a2996bbad00bc3f66bad32f28448397171
kebora <125854657+kebora@users.noreply.github.com>
2024-06-03 09:55:42 +0300

    feat: switch camera function

--- app/src/main/java/com/simiyudaniel/ishara2/MainActivity.java
+++ app/src/main/java/com/simiyudaniel/ishara2/MainActivity.java
@@ -1,5 +1,6 @@
 package com.simiyudaniel.ishara2;
 
+import org.opencv.android.Camera2Renderer;
 import org.opencv.android.CameraActivity;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
 import org.opencv.android.OpenCVLoader;
@@ -9,6 +10,7 @@ import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
 import org.opencv.core.Core;
 
 import android.content.Context;
+import android.hardware.Camera;
 import android.os.Bundle;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -20,14 +22,19 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.Collections;
 import java.util.List;
 
-public class MainActivity extends CameraActivity implements CvCameraViewListener2 {
+public class MainActivity extends CameraActivity implements CvCameraViewListener2,View.OnClickListener {
     private static final String TAG = "OCVSample::Activity";
 
     private CameraBridgeViewBase mOpenCvCameraView;
+
+    private int cameraIndex = CameraBridgeViewBase.CAMERA_ID_ANY;
 
     public MainActivity() {
         Log.i(TAG, "Instantiated new " + this.getClass());
@@ -56,6 +63,10 @@ public class MainActivity extends CameraActivity implements CvCameraViewListener
         mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
 
         mOpenCvCameraView.setCvCameraViewListener(this);
+
+        // Change the active camera
+        FloatingActionButton flipCameraBtn = (FloatingActionButton)findViewById(R.id.fab_flip_camera);
+        flipCameraBtn.setOnClickListener(this);
     }
 
     @Override
@@ -101,20 +112,47 @@ public class MainActivity extends CameraActivity implements CvCameraViewListener
         Core.rotate(inputFrame.rgba(),dest,Core.ROTATE_90_CLOCKWISE);
         return dest;
     }
+    //check for the availability of the front camera
+    private static boolean hasFrontCamera(Context context) {
+        if (context == null) return false;
+        // Check for at least one camera
+        int numberOfCameras = Camera.getNumberOfCameras();
+        if (numberOfCameras == 0) {
+            return false;
+        }
 
-//    private void switchCamera() {
-//        int cameraIndex = CameraBridgeViewBase.CAMERA_ID_ANY;
-//        if (cameraIndex == CameraBridgeViewBase.CAMERA_ID_BACK) {
-//            // First time opening camera, check for front camera availability
-//            cameraIndex = CameraBridgeViewBase.CAMERA_ID_FRONT;
-//        } else {
-//            // Switch between back and front camera
-//            cameraIndex = (cameraIndex == CameraBridgeViewBase.CAMERA_ID_FRONT) ? CameraBridgeViewBase.CAMERA_ID_BACK : CameraBridgeViewBase.CAMERA_ID_FRONT;
-//        }
-//
-//        mOpenCvCameraView.disableView();
-//        mOpenCvCameraView.setCameraIndex(cameraIndex);
-//        mOpenCvCameraView.enableView();
-//    }
+        // Loop through all cameras to see if any is front-facing
+        for (int i = 0; i < numberOfCameras; i++) {
+            Camera.CameraInfo info = new Camera.CameraInfo();
+            Camera.getCameraInfo(i, info);
+            if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+                return true;
+            }
+        }
+        return false;
+    }
+    // switch camera from front to back and vice versa
+    void switchCamera() {
+        if (cameraIndex == CameraBridgeViewBase.CAMERA_ID_ANY) {
+            // First time opening camera, check for front camera availability
+            if (hasFrontCamera(this)){
+                cameraIndex =CameraBridgeViewBase.CAMERA_ID_FRONT;
+            }
+        } else {
+            // Switch between back and front camera
+            cameraIndex = (cameraIndex == CameraBridgeViewBase.CAMERA_ID_FRONT) ? CameraBridgeViewBase.CAMERA_ID_BACK : CameraBridgeViewBase.CAMERA_ID_FRONT;
+        }
 
+        // Disable and re-enable camera view to apply changes
+        mOpenCvCameraView.disableView();
+        mOpenCvCameraView.setCameraIndex(cameraIndex);
+        mOpenCvCameraView.enableView();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.fab_flip_camera){
+            switchCamera();
+        }
+    }
 }
